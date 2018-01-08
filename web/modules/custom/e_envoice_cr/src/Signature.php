7452d2f6ea2dcbc93c19fd4e447eda5ec47140c0
mauricio <mauricio@bluetext.com>
2018-01-08 14:36:29 -0600

    Signature functionality.

--- web/modules/custom/e_envoice_cr/src/Signature.php
+++ web/modules/custom/e_envoice_cr/src/Signature.php
@@ -14,19 +14,25 @@ class Signature implements SignatureInterface {
     // locate where is the jar
     $res = chdir( "modules/custom/e_envoice_cr/jar" );
     if ($res) {
-      // define the paths
-      global $base_url;
-      $cert_path = "../../../../sites/default/files/certs/";
-      $settings = \Drupal::config('e_invoice_cr.settings');
-      $pass = $settings->get('cert_password');
-      $doc_path = "../../../../sites/default/files/xml/";
-      $signed_path = "../../../../sites/default/files/xml_signed/";
-      // build the java command
-      $command = 'java -jar xades4j-1.4.1-SNAPSHOT-jar-with-dependencies.jar ' . $cert_path . ' "'. $pass . '" ' . $doc_path . ' ' . $signed_path . ' 2>&1';
-      // execute the command
-      exec($command, $response);
-      // send the response
-      return $response;
+      $base_path = $_SERVER['DOCUMENT_ROOT'] . '/sites/default/files/';
+      // check the directory
+      $signed_path = $base_path . 'xml_signed/';
+      if (file_prepare_directory($signed_path, FILE_CREATE_DIRECTORY)) {
+        // define the paths
+        $cert_path = $base_path . "certs/";
+        $settings = \Drupal::config('e_invoice_cr.settings');
+        $pass = $settings->get('cert_password');
+        $doc_path = $base_path . "xml/";
+        $signed_path = $base_path . "xml_signed/";
+        // build the java command
+        $command = 'java -jar java-xades4j-signer.jar ' . $cert_path . ' "'. $pass . '" ' . $doc_path . ' ' . $signed_path . ' 2>&1';
+        // execute the command
+        exec($command, $response);
+        // send the response
+        return $response;
+      } else {
+        return false;
+      }
     } else {
       return false;
     }
