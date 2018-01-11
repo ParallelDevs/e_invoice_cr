936a68a26427f577d11ec4c3365a2d19c1c53cba
mauricio <mauricio@bluetext.com>
2018-01-11 16:40:25 -0600

    Fixed issue with disabled fields.

--- web/modules/custom/e_envoice_cr/modules/invoice_entity/js/invoice-rows.js
+++ web/modules/custom/e_envoice_cr/modules/invoice_entity/js/invoice-rows.js
@@ -10,12 +10,12 @@
   'use strict';
   Drupal.behaviors.geneteFields = {
     attach: function(context, settings) {
-      // calculate the fields needed
+      // Calculate the fields needed.
       calculateValues();
       calculateRowsTotal();
 
       function calculateValues() {
-        // check if it's possible to calculate the current fields
+        // Check if it's possible to calculate the current fields.
         var max = getRows();
         for (var i = 0; i < max; i++) {
           var quantity = $('input[data-drupal-selector=edit-field-filas-' + i + '-subform-field-cantidad-0-value]').val();
@@ -23,14 +23,14 @@
           if (quantity != '' && price != '') {
             var totalA = quantity * price;
             $('input[data-drupal-selector=edit-field-filas-' + i + '-subform-field-montototal-0-value]').val(totalA);
-            var tax = $('input[data-drupal-selector=edit-field-filas-' + i + '-subform-field-descuento-0-value]').val();
-            tax = '0.' + tax;
-            var totalWithTax = totalA + (totalA * tax);
+            var tax_id = $('select[data-drupal-selector=edit-field-filas-' + i + '-subform-field-impuesto]').val();
+            var tax = parseFloat(searchTax(tax_id));
+            var totalWithTax = totalA + ((tax/100)*totalA);
             $('input[data-drupal-selector=edit-field-filas-' + i + '-subform-field-subtotal-0-value]').val(totalWithTax);
             $('input[data-drupal-selector=edit-field-filas-' + i + '-subform-field-monto-total-linea-0-value]').val(totalWithTax);
           }
         }
-        // when there is updates on the fields
+        // When there is updates on the fields.
         $('input[id*="preciounitario"]').on('change', function () {
           var max = getRows();
           for (var i = 0; i < max; i++) {
@@ -39,18 +39,52 @@
             if (quantity != '' && price != '') {
               var totalA = quantity * price;
               $('input[data-drupal-selector=edit-field-filas-' + i + '-subform-field-montototal-0-value]').val(totalA);
-              var tax = $('input[data-drupal-selector=edit-field-filas-' + i + '-subform-field-descuento-0-value]').val();
-              tax = '0.' + tax;
-              var totalWithTax = totalA + (totalA * tax);
+              var tax_id = $('select[data-drupal-selector=edit-field-filas-' + i + '-subform-field-impuesto]').val();
+              var tax = parseFloat(searchTax(tax_id));
+              var totalWithTax = totalA + ((tax/100)*totalA);
               $('input[data-drupal-selector=edit-field-filas-' + i + '-subform-field-subtotal-0-value]').val(totalWithTax);
               $('input[data-drupal-selector=edit-field-filas-' + i + '-subform-field-monto-total-linea-0-value]').val(totalWithTax);
             }
           }
           calculateRowsTotal();
         });
+
+        // When the tax changes.
+        for (var i = 0; i < max; i++) {
+          $('select[data-drupal-selector=edit-field-filas-' + i + '-subform-field-impuesto]').on('change', function () {
+            for (var i = 0; i < max; i++) {
+              var quantity = $('input[data-drupal-selector=edit-field-filas-' + i + '-subform-field-cantidad-0-value]').val();
+              var price = $('input[data-drupal-selector=edit-field-filas-' + i + '-subform-field-preciounitario-0-value]').val();
+              if (quantity != '' && price != '') {
+                var totalA = quantity * price;
+                $('input[data-drupal-selector=edit-field-filas-' + i + '-subform-field-montototal-0-value]').val(totalA);
+                var tax_id = $('select[data-drupal-selector=edit-field-filas-' + i + '-subform-field-impuesto]').val();
+                var tax = parseFloat(searchTax(tax_id));
+                var totalWithTax = totalA + ((tax/100)*totalA);
+                $('input[data-drupal-selector=edit-field-filas-' + i + '-subform-field-subtotal-0-value]').val(totalWithTax);
+                $('input[data-drupal-selector=edit-field-filas-' + i + '-subform-field-monto-total-linea-0-value]').val(totalWithTax);
+              }
+            }
+            calculateRowsTotal();
+          });
+        }
       }
 
-      // calculate the general rows totals
+      // Gets the tax percentage
+      function searchTax(id) {
+        if (id == '_none') {
+          return 0;
+        } else {
+          var taxs = drupalSettings.taxsObject;
+          for (var i = 0; i < taxs.length; i++) {
+            if (taxs[""+i].id == id) {
+              return taxs[""+i].percentage;
+            }
+          }
+        }
+      }
+
+      // Calculate the general rows totals.
       function calculateRowsTotal() {
         var mt = $('input[id*="montototal"]');
         var st = $('input[id*="subtotal"]');
@@ -69,7 +103,7 @@
         $('#edit-field-totalcomprobante-0-value').val(totalInvoice);
       }
 
-      // returns the rows quantity
+      // Returns the rows quantity.
       function getRows() {
         var index = 0;
         while ($('input[data-drupal-selector=edit-field-filas-' + index + '-subform-field-preciounitario-0-value]').length > 0) {
