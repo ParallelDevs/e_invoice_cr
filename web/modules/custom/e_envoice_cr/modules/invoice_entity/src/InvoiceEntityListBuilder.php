faf540f7baf5523110bc120f13254867e4d7cf62
ochaves <andres1009_93@hotmail.com>
2018-01-18 16:12:43 -0600

    Fixes to some warining messages from jenkins.

--- web/modules/custom/e_envoice_cr/modules/invoice_entity/src/InvoiceEntityListBuilder.php
+++ web/modules/custom/e_envoice_cr/modules/invoice_entity/src/InvoiceEntityListBuilder.php
@@ -4,7 +4,6 @@ namespace Drupal\invoice_entity;
 
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityListBuilder;
-use Drupal\Core\Link;
 
 /**
  * Defines a class to build a listing of Invoice entities.
