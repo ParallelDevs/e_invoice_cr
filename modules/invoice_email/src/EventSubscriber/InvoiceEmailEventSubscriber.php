ad7d4d3b6ccaac6ffe127d7bee2a10ffb1f5845f
k-vargas <k.vargas@paralleldevs.com>
2018-11-05 15:28:19 -0600

    apply review changes

--- modules/invoice_email/src/EventSubscriber/InvoiceEmailEventSubscriber.php
+++ modules/invoice_email/src/EventSubscriber/InvoiceEmailEventSubscriber.php
@@ -37,7 +37,7 @@ class InvoiceEmailEventSubscriber implements EventSubscriberInterface {
       $entity = $em->getStorage("invoice_entity")->load($entityId);
       if (!is_null($entity) && !empty($entity->get("field_client")->getValue())) {
         // Define some required data.
-        $user_current = $entity->get('user_id')->getValue()[0]['target_id'];
+        $user_id = $entity->get('user_id')->getValue()[0]['target_id'];
         $invoice_id = $entity->get("field_numeric_key")->getValue()[0]['value'];
         $consecutive = $entity->get("field_consecutive_number")->getValue()[0]['value'];
         $invoice_date = $entity->get("field_invoice_date")->getValue()[0]['value'];
@@ -84,8 +84,8 @@ class InvoiceEmailEventSubscriber implements EventSubscriberInterface {
         }
 
         // Load the file entities.
-        $signed_file = File::load($this->searchFile('document-' . $user_current . '-' . $consecutive . 'segned.xml'));
-        $confirmation_file = File::load($this->searchFile('document-' . $user_current . '-' . $consecutive . 'confirmation.xml'));
+        $signed_file = File::load($this->searchFile('document-' . $user_id . '-' . $consecutive . 'segned.xml'));
+        $confirmation_file = File::load($this->searchFile('document-' . $user_id . '-' . $consecutive . 'confirmation.xml'));
 
         // Attach signed xml.
         $file = new \stdClass();
