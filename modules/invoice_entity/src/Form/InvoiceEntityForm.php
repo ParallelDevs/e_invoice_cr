dafb710637a0e5148870f63b8d6acb5a1d427fd9
k-vargas <k.vargas@paralleldevs.com>
2018-11-13 09:46:44 -0600

    add new validations to InvoiceReceivedEntityForm

--- modules/invoice_entity/src/Form/InvoiceEntityForm.php
+++ modules/invoice_entity/src/Form/InvoiceEntityForm.php
@@ -13,6 +13,7 @@ use Drupal\e_invoice_cr\Signature;
 use Drupal\e_invoice_cr\XMLGenerator;
 use Drupal\invoice_entity\Entity\InvoiceEntityInterface;
 use Drupal\tax_entity\Entity\TaxEntity;
+use Drupal\invoice_entity\Entity\InvoiceEntity;
 
 /**
  * Form controller for Invoice edit forms.
